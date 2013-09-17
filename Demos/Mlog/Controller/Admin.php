c60090ff49584727ec88a7e2a018102b0b7e5091
MaGuowei <imaguowei@gmail.com>
2013-09-17 10:42:51 +0800

    更新readme

---
+++ Demos/Mlog/Controller/Admin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link https://github.com/MaGuowei
+ * @copyright 2013 maguowei.com
+ * @author Ma Guowei <imaguowei@gmail.com>
+ */
+
+namespace Mlog\Controller;
+
+/**
+ * Class Admin
+ * @package Mlog\Controller
+ */
+class Admin
+{
+    public function index()
+    {
+
+    }
+
+    public function user()
+    {
+
+    }
+
+    public function setting()
+    {
+
+    }
+}
